708e3d219f487006bfa8060a15ff270d5caf4444
lyhhh <17610603221@163.com>
2020-11-08 02:54:50 +0800

    引入 mobx

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Loading from './components/Loading'
 const Home = lazy(() => import('./pages/Home'))
 const History = lazy(() => import('./pages/History'))
 const About = lazy(() => import('./pages/About'))
+const Login = lazy(() => import('./pages/Login'))
+const Register = lazy(() => import('./pages/Register'))
+
 
 function App() {
 	return (
@@ -20,6 +23,8 @@ function App() {
 						<Route path="/" exact component={Home}/>
 						<Route path="/history" component={History}/>
 						<Route path="/about" component={About}/>
+						<Route path="/login" component={Login}/>
+						<Route path="/register" component={Register}/>
 					</Switch>
 				</Suspense>
 			</main>
