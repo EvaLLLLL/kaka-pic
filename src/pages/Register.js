33c21e014df69ce7d35b74ec1512b05deee7d534
lyhhh <17610603221@163.com>
2020-11-08 05:15:10 +0800

    引入 antd 表单组件

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,15 +1,87 @@
-import React from 'react'
-import {observer} from 'mobx-react'
-import {useStores} from '../stores/index'
+import React from "react";
+import styled from "styled-components";
+import { Form, Input, Button} from "antd";
 
-const Component = observer(() => {
-	const {AuthStore} = useStores()
-	
-	return (
-		<>
-			<h1>Register: {AuthStore.values.username}</h1>
-		</>
-	)
-})
+const Wrapper = styled.div`
+  max-width: 600px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 20px;
+`;
 
-export default Component
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 12,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 12,
+  },
+};
+
+const Component = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Wrapper>
+      <Title>注册</Title>
+      <Form
+        {...layout}
+        name="basic"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "请输入用户名",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "请输入密码",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default Component;
