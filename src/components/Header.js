04d2b1a59fc12b61bbb4220f7f4e6b5677f82216
lyhhh <17610603221@163.com>
2020-11-08 03:29:00 +0800

    使用 mobx 中的数据与方法

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,10 +15,16 @@ const LogoUrl = styled.img`
 const StyledLink = styled(NavLink)`
 	color: #fff;
 	margin-left: 30px;
-	
 	&.active {
 		border-bottom: 1px solid #ccc;
 	}
+`
+const Login = styled.div`
+	display: inline-block;
+	margin-left: 200px;
+`
+const Button = styled.button`
+	margin-left: 10px;
 `
 
 function Component() {
@@ -29,6 +35,14 @@ function Component() {
 				<StyledLink to="/" activeClassName="active" exact>首页</StyledLink>
 				<StyledLink to="/history" activeClassName="active">历史</StyledLink>
 				<StyledLink to="/about" activeClassName="active">关于我</StyledLink>
+				<Login>
+					<Button>
+						<StyledLink to="/login">登录</StyledLink>
+					</Button>
+					<Button>
+						<StyledLink to="/register">注册</StyledLink>
+					</Button>
+				</Login>
 			</nav>
 		</Header>
 	)
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {observer} from 'mobx-react'
 import {useStores} from '../stores/index'
 
 const Component = observer(() => {
 	const {AuthStore} = useStores()
+	const inputRef = useRef(null)
+	
+	const bindChange = () => {
+		AuthStore.setUsername(inputRef.current.value)
+	}
 	
 	return (
 		<>
 			<h1>Login: {AuthStore.values.username}</h1>
+			<input onChange={bindChange} ref={inputRef}/>
 		</>
 	)
 })
