085ff51b9e0c8a97c58f6fef1b4a850c35b9e532
lyhhh <17610603221@163.com>
2020-11-08 01:45:56 +0800

    搭建页面结构

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import History from './pages/History'
+import About from './pages/About'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Header/>
+			<Switch>
+				<Route path="/" exact>
+					<Home/>
+				</Route>
+				<Route path="/history">
+					<History/>
+				</Route>
+				<Route path="/about">
+					<About/>
+				</Route>
+			</Switch>
+			<Footer/>
+		</div>
+	)
 }
 
-export default App;
+export default App
