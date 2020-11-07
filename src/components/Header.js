11647b6c6a7606a73ec3c5d23707c6c0f2c9e3eb
lyhhh <17610603221@163.com>
2020-11-08 04:46:19 +0800

    引入 antd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,73 @@
-import React from 'react'
-import Logo from './logo.svg'
-import {NavLink} from 'react-router-dom'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import Logo from "./logo.svg";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { Button } from "antd";
 
 const Header = styled.header`
-	padding: 10px 100px;
-	display: flex;
-	align-items: center;
-	background-color: #001528;
-`
+  padding: 10px 100px;
+  display: flex;
+  align-items: center;
+  background-color: #001528;
+`;
 const LogoUrl = styled.img`
-	height: 30px;
-`
+  height: 30px;
+`;
 const StyledLink = styled(NavLink)`
-	color: #fff;
-	margin-left: 30px;
-	&.active {
-		border-bottom: 1px solid #ccc;
-	}
-`
+  color: #fff;
+  margin-left: 30px;
+  &.active {
+    border-bottom: 1px solid #ccc;
+  }
+`;
 const Login = styled.div`
-	display: inline-block;
-	margin-left: 200px;
-`
-const Button = styled.button`
-	margin-left: 10px;
-`
+  margin-left: auto;
+`;
+const StyledButton = styled(Button)`
+  margin-left: 10px;
+`;
 
 function Component() {
-	return (
-		<Header>
-			<LogoUrl src={Logo} alt="logo"/>
-			<nav>
-				<StyledLink to="/" activeClassName="active" exact>首页</StyledLink>
-				<StyledLink to="/history" activeClassName="active">历史</StyledLink>
-				<StyledLink to="/about" activeClassName="active">关于我</StyledLink>
-				<Login>
-					<Button>
-						<StyledLink to="/login">登录</StyledLink>
-					</Button>
-					<Button>
-						<StyledLink to="/register">注册</StyledLink>
-					</Button>
-				</Login>
-			</nav>
-		</Header>
-	)
+  const [isLogin, setIsLogin] = useState(false);
+
+  return (
+    <Header>
+      <LogoUrl src={Logo} alt="logo" />
+      <nav>
+        <StyledLink to="/" activeClassName="active" exact>
+          首页
+        </StyledLink>
+        <StyledLink to="/history" activeClassName="active">
+          历史
+        </StyledLink>
+        <StyledLink to="/about" activeClassName="active">
+          关于我
+        </StyledLink>
+      </nav>
+      <Login>
+        {isLogin ? (
+          <>
+            EvaL{" "}
+            <StyledButton type="primary" onClick={() => setIsLogin(false)}>
+              注销
+            </StyledButton>
+          </>
+        ) : (
+          <>
+            <StyledButton
+              type="primary"
+              onClick={() => {
+                setIsLogin(true);
+              }}
+            >
+              登录
+            </StyledButton>
+            <StyledButton type="primary">注册</StyledButton>
+          </>
+        )}
+      </Login>
+    </Header>
+  );
 }
 
-export default Component
+export default Component;
