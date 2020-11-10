687720da04a49273829b07d14bc289073e48127b
lyhhh <17610603221@163.com>
2020-11-10 15:20:23 +0800

    完成上传结果 HTML

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,39 +1,56 @@
-import React from "react";
-import { useStores } from "../stores";
-import { observer } from "mobx-react";
-import { Upload, message } from "antd";
-import { InboxOutlined } from "@ant-design/icons";
+import React from 'react'
+import {useStores} from '../stores'
+import {observer} from 'mobx-react'
+import {Upload, message} from 'antd'
+import {InboxOutlined} from '@ant-design/icons'
+import styled from 'styled-components'
+
+const Result = styled.div`
+  margin-top: 30px;
+  border: 1px dashed #ccc;
+  padding: 20px;
+`
+
+const H1 = styled.h1`
+  margin: 20px 0;
+  text-align: center;
+`
+
+const Img = styled.img`
+  max-width: 300px;
+
+`
 
 const Component = observer(() => {
-	const { ImageStore, UserStore } = useStores();
-	const { Dragger } = Upload;
-
+	const {ImageStore, UserStore} = useStores()
+	const {Dragger} = Upload
+	
 	const props = {
 		showUploadList: false,
 		beforeUpload: (file) => {
-			ImageStore.setFile(file);
-			ImageStore.setFilename(file.name);
+			ImageStore.setFile(file)
+			ImageStore.setFilename(file.name)
 			if (UserStore.currentUser === null) {
-				message.warning("请先登录再上传！");
-				return false;
+				message.warning('请先登录再上传！')
+				return false
 			}
 			ImageStore.upLoad()
 				.then((serverFile) => {
-					console.dir(serverFile);
-					console.log("上传成功");
+					console.dir(serverFile)
+					console.log('上传成功')
 				})
 				.catch(() => {
-					console.error("上传失败");
-				});
-			return false;
+					console.error('上传失败')
+				})
+			return false
 		},
-	};
-
+	}
+	
 	return (
-		<div>
+		<Result>
 			<Dragger {...props}>
 				<p className="ant-upload-drag-icon">
-					<InboxOutlined />
+					<InboxOutlined/>
 				</p>
 				<p className="ant-upload-text">Click or drag file to this area to upload</p>
 				<p className="ant-upload-hint">
@@ -41,16 +58,28 @@ const Component = observer(() => {
 					company data or other band files
 				</p>
 			</Dragger>
-			<div>
-				<h1>上传结果</h1>
-				{ImageStore.serverFile ? (
-					<div>{ImageStore.serverFile.attributes.url.attributes.url}</div>
-				) : (
-					<></>
-				)}
-			</div>
-		</div>
-	);
-});
+			{ImageStore.serverFile ? (
+				<Result>
+					<H1>上传结果</H1>
+					<dl>
+						<dt>线上地址</dt>
+						<dd><a href={ImageStore.serverFile.attributes.url.attributes.url}
+						       target="_blank">{ImageStore.serverFile.attributes.url.attributes.url}</a></dd>
+						<dt>文件名</dt>
+						<dd>{ImageStore.filename}</dd>
+						<dt>图片预览</dt>
+						<dd><Img src={ImageStore.serverFile.attributes.url.attributes.url}/></dd>
+					</dl>
+					<dt>尺寸定制</dt>
+					<dd>更多尺寸</dd>
+					<dt>
+						<input placeholder="最大宽度（可选）"/>
+						<input placeholder="最大高度（可选）"/>
+					</dt>
+				</Result>
+			) : null}
+		</Result>
+	)
+})
 
-export default Component;
+export default Component
