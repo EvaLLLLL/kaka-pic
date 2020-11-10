61b342e39d03c8b93ff1af1b13f8892ccb3aabce
lyhhh <17610603221@163.com>
2020-11-10 14:51:25 +0800

    未登录上传进行警告

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,54 +1,56 @@
 import React from "react";
 import { useStores } from "../stores";
 import { observer } from "mobx-react";
-import { Upload } from "antd";
+import { Upload, message } from "antd";
 import { InboxOutlined } from "@ant-design/icons";
 
 const Component = observer(() => {
-  const { ImageStore } = useStores();
-  const { Dragger } = Upload;
+	const { ImageStore, UserStore } = useStores();
+	const { Dragger } = Upload;
 
-  const props = {
-    showUploadList: false,
-    beforeUpload: (file) => {
-      ImageStore.setFile(file);
-      ImageStore.setFilename(file.name);
-      ImageStore.upLoad()
-        .then((serverFile) => {
-          console.dir(serverFile);
-          console.log("上传成功");
-        })
-        .catch(() => {
-          console.error("上传失败");
-        });
-      return false;
-    },
-  };
+	const props = {
+		showUploadList: false,
+		beforeUpload: (file) => {
+			ImageStore.setFile(file);
+			ImageStore.setFilename(file.name);
+			if (UserStore.currentUser === null) {
+				message.warning("请先登录再上传！");
+				return false;
+			}
+			ImageStore.upLoad()
+				.then((serverFile) => {
+					console.dir(serverFile);
+					console.log("上传成功");
+				})
+				.catch(() => {
+					console.error("上传失败");
+				});
+			return false;
+		},
+	};
 
-  return (
-    <div>
-      <Dragger {...props}>
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">
-          Click or drag file to this area to upload
-        </p>
-        <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibit from uploading
-          company data or other band files
-        </p>
-      </Dragger>
-      <div>
-        <h1>上传结果</h1>
-        {ImageStore.serverFile ? (
-          <div>{ImageStore.serverFile.attributes.url.attributes.url}</div>
-        ) : (
-          <></>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Dragger {...props}>
+				<p className="ant-upload-drag-icon">
+					<InboxOutlined />
+				</p>
+				<p className="ant-upload-text">Click or drag file to this area to upload</p>
+				<p className="ant-upload-hint">
+					Support for a single or bulk upload. Strictly prohibit from uploading
+					company data or other band files
+				</p>
+			</Dragger>
+			<div>
+				<h1>上传结果</h1>
+				{ImageStore.serverFile ? (
+					<div>{ImageStore.serverFile.attributes.url.attributes.url}</div>
+				) : (
+					<></>
+				)}
+			</div>
+		</div>
+	);
 });
 
 export default Component;
